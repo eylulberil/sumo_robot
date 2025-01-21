65b20d22f2dc16bf6d65d7fd2c7b202024198eaa
Eylül Beril Yurttaş <133544514+eylulberil@users.noreply.github.com>
2025-01-21 15:37:44 +0100

    Add files via upload

--- sumo_robot.ino
+++ sumo_robot.ino
@@ -1,26 +1,42 @@
-#define INF_PIN B3
-#define TCRT_PIN 2
-#define START_BTN 7
+#define TCRT_PIN PB6
+#define START_BTN PA1
+
 struct Motor {
   int in1_pin;
   int in2_pin;
 };
 
+Motor leftMotor = {PA0, PA1};
+Motor rightMotor = {PA2, PA3};
 
-Motor leftMotor = {A0, A1};  // IN1, IN2
-Motor rightMotor = {A2, A3};
+int radar_mode_turn_direction = 1;
+
+struct INFSENSOR {
+  int right_pin;
+  int middle_pin;
+  int left_pin;
+};
+
+INFSENSOR infra_pin = {PB3, PB4, PB5}; //right, middle, left
+
 
 unsigned long currentTime = 0, turn_start_time = 0;
 bool is_radar_mode = false;
 
 void setup() {
-  pinMode(INF_PIN, INPUT);
+  pinMode(infra_pin.right_pin, INPUT);
+  pinMode(infra_pin.middle_pin, INPUT);
+  pinMode(infra_pin.left_pin, INPUT);
   pinMode(TCRT_PIN, INPUT);
   pinMode(SRT_BTN, INPUT);
   pinMode(leftMotor.in1_pin, OUTPUT);
   pinMode(leftMotor.in2_pin, OUTPUT);
+  digitalWrite(leftMotor.in1_pin, LOW);
+  digitalWrite(leftMotor.in2_pin, LOW);
   pinMode(rightMotor.in1_pin, OUTPUT);
   pinMode(rightMotor.in2_pin, OUTPUT);
+  digitalWrite(rightMotor.in1_pin, LOW);
+  digitalWrite(rightMotor.in2_pin, LOW);
   currentTime = millis();
   turn_start_time = millis();
 }
@@ -33,15 +49,21 @@ void loop() {
   delay(5000);
   while(1) { //fight started
   currentTime = millis();
-    if(digitalRead(INF_PIN) == 1) {
+    if(digitalRead(infra_pin.middle_pin) == 1) {
       is_radar_mode = false;
       moveTank(100, 100);
     } else {
       if(!is_radar_mode) {
         turn_start_time = millis();
       }
+      if(digitalRead(infra_pin.right_pin) == 1) {
+        radar_mode_turn_direction = 1;
+      }
+      if(digitalRead(infra_pin.left_pin) == 1) {
+        radar_mode_turn_direction = -1;
+      }
       is_radar_mode = true;
-      moveTank(100, -100);
+      moveTank(100*radar_mode_turn_direction, -100*radar_mode_turn_direction);
       if(currentTime - turn_start_time > 3000)
         break;
     }
@@ -61,23 +83,19 @@ void loop() {
 void moveTank(int power_left, int power_right) {
   
   if(power_left < 0) {
-    //power_left = power_left*-1;
     digitalWrite(leftMotor.in1_pin, LOW);
     digitalWrite(leftMotor.in2_pin, HIGH);
   } else {
     digitalWrite(leftMotor.in2_pin, LOW);
     digitalWrite(leftMotor.in1_pin, HIGH);
   }
-  //analogWrite(leftMotor.pwm_pin, round(power_left/2.55));
 
 
     if(power_right < 0) {
-    //power_right = power_right*-1;
     digitalWrite(rightMotor.in1_pin, LOW);
     digitalWrite(rightMotor.in2_pin, HIGH);
   } else {
     digitalWrite(rightMotor.in2_pin, LOW);
     digitalWrite(rightMotor.in1_pin, HIGH);
   }
-  //analogWrite(rightMotor.pwm_pin, round(power_right/2.55));
 }
